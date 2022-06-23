ff72b2905941950c9fadf0f9843b84eda57d944c
admin <mr.nobby009@gmail.com>
2022-06-23 13:57:04 +0530

    23/6

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_login/class/Auth.dart';
 import 'package:flutter/material.dart';
-
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,11 +10,70 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Text("Home"),
+      ),
+      body: Card(
+        color: Colors.pinkAccent.shade100,
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Card(
+                color: Colors.pink,
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                      child: Text(
+                    "Email :- ${auth.currentUser!.email}",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  )),
+                ),
+              ),
+              Card(
+                color: Colors.pink,
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                      child: Text(
+                    "UID :- ${auth.currentUser!.uid}",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  )),
+                ),
+              ),
+              Expanded(
+                child: Card(),
+              ),
+              InkWell(
+                onTap: (){
+                  Auth().SingOut(context);
+                },
+                child: Card(
+                  color: Colors.pink,
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(
+                        child: Text(
+                      "Sing Out",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    )),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
